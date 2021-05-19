cfea005a92531aa82eaa4b0ecba200c36f2718d6
Nyi Nyi Htun Lwin <nnhl.mgk@gmail.com>
2021-05-19 22:16:55 +0630

    update : Custom markers added

--- app/src/main/java/com/nyinyihtunlwin/prj_getgo/MapsActivity.kt
+++ app/src/main/java/com/nyinyihtunlwin/prj_getgo/MapsActivity.kt
@@ -1,12 +1,22 @@
 package com.nyinyihtunlwin.prj_getgo
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.PorterDuff
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.MarkerOptions
 import com.nyinyihtunlwin.prj_getgo.databinding.ActivityMapsBinding
 import com.nyinyihtunlwin.prj_getgo.search.SearchResultsActivity
@@ -32,21 +42,59 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val mCustomMarker = (getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater)
+            .inflate(
+                R.layout.view_custom_marker, null
+            )
+
+        val builder = LatLngBounds.builder()
+        val loc1 = LatLng(16.87644, 96.17150)
+        val marker1 = mMap.addMarker(
+            MarkerOptions().position(loc1)
+                .icon(BitmapDescriptorFactory.fromBitmap(getMarkerFromView(mCustomMarker, "2")))
+        )
+        val loc2 = LatLng(16.894784, 96.158052)
+        val marker2 = mMap.addMarker(
+            MarkerOptions().position(loc2)
+                .icon(BitmapDescriptorFactory.fromBitmap(getMarkerFromView(mCustomMarker, "3")))
+        )
+
+        val loc3 = LatLng(16.886238, 96.188625)
+        val marker3 = mMap.addMarker(
+            MarkerOptions().position(loc3)
+                .icon(BitmapDescriptorFactory.fromBitmap(getMarkerFromView(mCustomMarker, "5")))
+        )
+        builder.include(marker1.position)
+        builder.include(marker2.position)
+        builder.include(marker3.position)
+        //  mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(loc1, 14F))
+
+        val bounds = builder.build()
+        val width = resources.displayMetrics.widthPixels
+        val height = resources.displayMetrics.heightPixels
+        val padding =
+            (width * 0.12).toInt() // offset from edges of the map 12% of screen
+        val cu = CameraUpdateFactory.newLatLngBounds(bounds, width, height, padding)
+        mMap.moveCamera(cu)
+
+    }
+
+    private fun getMarkerFromView(view: View, count: String): Bitmap {
+        val tvCount: TextView = view.findViewById(R.id.tvCount)
+        tvCount.text = count
+        view.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED)
+        view.layout(0, 0, view.measuredWidth, view.measuredHeight)
+        view.buildDrawingCache()
+        val bitmap =
+            Bitmap.createBitmap(view.measuredWidth, view.measuredHeight, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        canvas.drawColor(Color.WHITE, PorterDuff.Mode.SRC_IN)
+        val drawable = view.background
+        drawable?.draw(canvas)
+        view.draw(canvas)
+        return bitmap
     }
 }
